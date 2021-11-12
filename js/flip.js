92d4712f475251d6c8d32e6e72a7b64e58fe9e24
marukosan93 <watashiwamarukosandesu@gmail.com>
2021-11-12 08:35:32 +0200

    first website draft

---
+++ js/flip.js
@@ -0,0 +1,5 @@
+  //flip card
+    $(".card").click(function(e){
+    e.preventDefault();
+    $(this).toggleClass('is-flipped');
+})
---
+++ js/main.js
@@ -0,0 +1,41 @@
+(function ($) {
+    "use strict";
+    // Initiate the wowjs
+    new WOW().init();
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.navbar').addClass('nav-sticky');
+        } else {
+            $('.navbar').removeClass('nav-sticky');
+        }
+    });
+
+
+    // News carousel
+    $(".news-carousel").owlCarousel({
+        autoplay: true,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+})(jQuery);
