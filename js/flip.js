ed51f2a4e49fb32afcca70d16f4b7de4819cf85f
marukosan93 <watashiwamarukosandesu@gmail.com>
2021-11-12 08:56:26 +0200

    first website draft

--- js/flip.js
+++
@@ -1,5 +0,0 @@
-  //flip card
-    $(".card").click(function(e){
-    e.preventDefault();
-    $(this).toggleClass('is-flipped');
-})
---
+++ js/wownoblink.js
@@ -0,0 +1,4 @@
+  var wows = document.getElementsByClassName('wow')
+  for (i = 0; i < wows.length; i++){
+    wows[i].style.visibility ='hidden';
+  }
